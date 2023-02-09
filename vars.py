dcbbf1857125b847d42c85e22e85fa388aff4c92
carter3647 <118304945+carter3647@users.noreply.github.com>
2023-02-09 16:14:49 -0500

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,24 @@
+#the system variables required to run InfinityOS
+import os
+def cls():
+    os.system(cls)
+def save():
+    file1.write(username+"\n")
+    file1.write(ca+"\n")
+    file1.write(T1+"\n")
+    file1.write(T2+"\n")
+    file1.write(T3+"\n")
+    file1.close()
+unum=1
+ufile="3"
+logusr="placeholder"
+username="placeholder"
+getusernum=0
+d=False
+L1=2
+p="placeholder"
+ca="placeholder"
+ci="placeholder"
+file1="write"
+commands=["[draw something]","[texedit]","[calculator]","[build]","[logoff]"]
+startin=""
